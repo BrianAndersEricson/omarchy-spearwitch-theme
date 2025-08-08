92b0d641f63457c664411b5897bc25d9cfa93fcd
Brian Ericson <brianericson@pm.me>
2025-08-07 21:47:00 -0400

    Initialize repo for theme and upload theme files

---
+++ neovim.lua
@@ -0,0 +1,10 @@
+-- Spearwitch Theme for Neovim
+-- Custom colorscheme based on rootloops.sh palette
+return {
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "spearwitch", -- Custom Spearwitch colorscheme
+		},
+	},
+}
